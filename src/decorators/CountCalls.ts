79628156c848a4212bea999e768dddcff4a273d5
Erik Nygren <erik.nygren@appliedtechnology.se>
2020-04-05 02:27:48 +0200

    pending further research

--- src/decorators/CountCalls.ts
+++ src/decorators/CountCalls.ts
@@ -16,24 +16,24 @@ const methodDecorator = <T, K extends keyof T>(_target: T, _key: K, descriptor: 
 ////////////////////////////////////////////////////////////////////////////////////////
 
 const constructorCallHandler = {
-  construct(target: Constructor, argumentsList: any[], _newTarget: FunctionConstructor) {
+  construct(target: Constructor, argumentsList: any[], _newTarget: Object) {
     count(`${target.name}: constructor`);
     return new target(...argumentsList);
   },
 };
 
-const constructorDecorator = <T extends Constructor>(constructor: T): InstanceType<ProxyConstructor> => {
+const constructorDecorator = (constructor: Constructor) => {
   return new Proxy(constructor, constructorCallHandler);
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-const clientAPI = <T>(...args: any[]): Constructor<T> | PropertyDescriptor => {
+const clientAPI = (...args: any[]): any | never => {
   switch (args.length) {
     case 1:
-      return constructorDecorator<Constructor<T>>(args[0]);
+      return constructorDecorator(args[0]);
     case 3:
-      return methodDecorator<T, keyof T>(args[0], args[1], args[2]);
+      return methodDecorator(args[0], args[1], args[2]);
     default:
       throw new RangeError(`Expected 1 or 3 arguments, recieved ${args.length}!`);
   }
