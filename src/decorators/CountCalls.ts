655dcab6ff14d62bf91f3e3ef4c896529d98dcfc
Erik Nygren <erik.nygren@appliedtechnology.se>
2020-04-05 09:35:16 +0200

    imrpove type safety

--- src/decorators/CountCalls.ts
+++ src/decorators/CountCalls.ts
@@ -2,15 +2,15 @@ import { isConstructorMethod } from "../predicates/decorators/class/constructor"
 import { Constructor } from "../typings/declarations";
 import { count } from "../utils/console";
 
-const countCalls = {
-  apply(target: Function, thisArg: Object, argumentsList: any[]) {
+const countCalls: ProxyHandler<Function> = {
+  apply(target: Function, thisArg: Object, argumentsList: unknown[]) {
     count(`${thisArg.constructor.name}: ${target.name}`);
     return target.apply(thisArg, argumentsList);
   },
 };
 
-const countConstructorCalls = {
-  construct(target: Constructor, argumentsList: any[], _newTarget: Object) {
+const countConstructorCalls: ProxyHandler<Constructor> = {
+  construct(target: Constructor, argumentsList: unknown[], _newTarget: Object) {
     count(`${target.name}: constructor`);
     return new target(...argumentsList);
   },
@@ -21,9 +21,9 @@ const countConstructorCalls = {
 const prototypeDecorator = (targetClass: any) => {
   const descriptors = Object.getOwnPropertyDescriptors(targetClass.prototype);
 
-  let decoratedConstructor;
+  let decoratedConstructor: Constructor | void;
   for (let descriptor in descriptors) {
-    const method = targetClass.prototype[descriptor];
+    const method: Function = targetClass.prototype[descriptor];
 
     if (isConstructorMethod(method, targetClass)) {
       /** This means we've identified the constructor (which requires a custom trap handler). */
--- src/predicates/decorators/class/constructor.ts
+++ src/predicates/decorators/class/constructor.ts
@@ -1,10 +1,9 @@
 import { Constructor } from "../../../typings/declarations";
 
-const isConstructorMethod = (method: Function, targetClass: any): method is Constructor => {
+const isConstructorMethod = (method: Function, targetClass: Constructor): method is Constructor => {
   const methodPrototype = Object.getPrototypeOf(method);
   const classPrototype = Object.getPrototypeOf(targetClass);
   return Object.is(methodPrototype, classPrototype);
 };
 
 export { isConstructorMethod };
-
