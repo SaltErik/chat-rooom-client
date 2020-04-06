4787b637ac5385e25298237a96e03472cc205a72
Erik Nygren <erik.nygren@appliedtechnology.se>
2020-04-06 09:31:58 +0200

    extract temp

--- src/decorators/CountCalls.ts
+++ src/decorators/CountCalls.ts
@@ -4,18 +4,19 @@ import { countCalls } from "./handlers/countCalls";
 import { countConstructorCalls } from "./handlers/countConstructorCalls";
 
 const prototypeDecorator = (targetClass: any) => {
-  const descriptors = Object.getOwnPropertyDescriptors(targetClass.prototype);
+  const classPrototype = targetClass.prototype;
+  const descriptors = Object.getOwnPropertyDescriptors(classPrototype);
 
   let decoratedConstructor: Constructor | void;
   for (let descriptor in descriptors) {
-    const method: Function = targetClass.prototype[descriptor];
+    const method: Function = classPrototype[descriptor];
 
     if (isConstructorMethod(method, targetClass)) {
       /** This means we've identified the constructor (which requires a custom trap handler). */
       decoratedConstructor = new Proxy(method, countConstructorCalls);
     } else {
       /** Regular method, regular trap handler. */
-      targetClass.prototype[descriptor] = new Proxy(method, countCalls);
+      classPrototype[descriptor] = new Proxy(method, countCalls);
     }
   }
 
