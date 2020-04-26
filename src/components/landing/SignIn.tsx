0ef39016f235fb8ebe534776721c987968107788
Erik Nygren <erik.nygren@appliedtechnology.se>
2020-04-26 17:27:25 +0200

    minor cleanup

--- src/components/landing/SignIn.tsx
+++ src/components/landing/SignIn.tsx
@@ -20,15 +20,10 @@ const SignIn: FC<Props> = ({ onChange, onSubmit, mirror }): JSX.Element => {
           Password
         </label>
         <input className="form-control" id="inputPassword" type="password" placeholder="Password" required></input>
-        <div className="checkbox mb-3">
-          <label>{/* <input type="checkbox" value="remember-me">
-                Remember me
-              </input> */}</label>
-        </div>
+        <div className="checkbox mb-3"></div>
         <button className="btn btn-lg btn-primary btn-block" type="submit">
           Sign in
         </button>
-        <p className="mt-5 mb-3 text-muted">&copy;2020</p>
       </form>
     </div>
   );
